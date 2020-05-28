e8cce9b1ce1daff1e7cfbcce0b811aa31fdf4c52
Chad <chad.spector@mail.mcgill.ca>
2020-05-28 17:39:40 -0400

    Youtube API fully set up

--- YoutubeSearch/McViewer/views.py
+++ YoutubeSearch/McViewer/views.py
@@ -1,6 +1,53 @@
+import requests
+
+from isodate import parse_duration
+
 from django.shortcuts import render
+from django.config import settings
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'McViewer/login.html')
+    search_url = 'https://www.googleapis.com/youtube/v3/search'
+    video_url = 'https://www.googleapis.com/youtube/v3/videos'
+
+     search_params = {
+        'part' : 'snippet',
+        'q' : 'learn python',
+        'key' : settings.YOUTUBE_API_KEY,
+        'maxResults' : 9,
+        'type' : 'video'
+    }
+    video_ids = []
+    res = requests.get(search_url, params = search_params)
+    
+    results = res.json()['items']
+
+    for results in results:
+        video_ids.append(result['id']['videoId'])    
+
+    video_params = {
+        'key' : settings.YOUTUBE_API_KEY,
+        'part' : 'snippet,contentDetails',
+        'id' : ','.join(video_ids),
+        'maxResults' : 9
+    }
+    res = requests.get(search_url, params = video_params)
+
+    results = res.json()['items']
+
+    videos = []
+    for results in results:
+        video_data = {
+            'title' : result['snippet']['title'],
+            'id' : result['id']
+            'duration' : parse_duration(result['contentDetails']['duration'])
+            'thumbnail' : result['snippet']['thumbnails']['high']['url']
+        }
+        videos.append(video_data)
+    
+    context = {
+        'videos' : videos
+    }
+
+    return render(request, 'McViewer/login.html', context)
 # Create your views here.
