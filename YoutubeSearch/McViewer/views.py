05e2118f804efbf0ad7e95a76da4bae912ae1de8
Chad <chad.spector@mail.mcgill.ca>
2020-06-01 19:39:58 -0400

    everything good

--- YoutubeSearch/McViewer/views.py
+++ YoutubeSearch/McViewer/views.py
@@ -3,10 +3,10 @@ import requests
 from isodate import parse_duration
 
 from django.shortcuts import render
-from django.config import settings
+from django.conf import settings
 from django.http import HttpResponse
 
-def index(request):
+def search(request):
     search_url = 'https://www.googleapis.com/youtube/v3/search'
     video_url = 'https://www.googleapis.com/youtube/v3/videos'
 
@@ -14,33 +14,33 @@ def index(request):
         'part' : 'snippet',
         'q' : 'learn python',
         'key' : settings.YOUTUBE_API_KEY,
-        'maxResults' : 9,
+        'maxResults' : 6,
         'type' : 'video'
     }
     video_ids = []
     res = requests.get(search_url, params = search_params)
     
-    results = res.json()['items']
-
-    for results in results:
-        video_ids.append(result['id']['videoId'])    
-
+    search_results = res.json()['items']
+    for result in search_results:
+        video_ids.append(result['id']['videoId'])
+    
+    
     video_params = {
         'key' : settings.YOUTUBE_API_KEY,
         'part' : 'snippet,contentDetails',
         'id' : ','.join(video_ids),
-        'maxResults' : 9
+        'maxResults' : 6
     }
-    res = requests.get(search_url, params = video_params)
+    res = requests.get(video_url, params = video_params)
 
-    results = res.json()['items']
+    video_results = res.json()['items']
 
     videos = []
-    for results in results:
+    for result in video_results:
         video_data = {
             'title' : result['snippet']['title'],
-            'id' : result['id']
-            'duration' : parse_duration(result['contentDetails']['duration'])
+            'id' : result['id'],
+            'duration' : parse_duration(result['contentDetails']['duration']),
             'thumbnail' : result['snippet']['thumbnails']['high']['url']
         }
         videos.append(video_data)
@@ -49,9 +49,8 @@ def index(request):
         'videos' : videos
     }
 
-    return render(request, 'McViewer/login.html', context)
+    return render(request, 'home_page.html', context)
 # Create your views here.
 def login(request):
-    return render(request, 'McViewer/login.html')
+    return render(request, 'login.html')
 # Create your views here.
-
