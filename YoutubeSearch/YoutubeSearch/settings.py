cde5307ad1068092818c312a1716f4ac0d7c5028
Chad <chad.spector@mail.mcgill.ca>
2020-06-01 20:05:32 -0400

    fixed profile picture display

--- YoutubeSearch/YoutubeSearch/settings.py
+++ YoutubeSearch/YoutubeSearch/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'McViewer/media')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
