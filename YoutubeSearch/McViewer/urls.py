45172c9e58259212bfa8791251c8da0e3a5eba56
Daniel Bensoussan <danielbensoussan@Daniels-MacBook-Pro.local>
2020-06-07 16:55:26 -0400

    git ignore finally added hopefully

--- YoutubeSearch/McViewer/urls.py
+++ YoutubeSearch/McViewer/urls.py
@@ -11,9 +11,6 @@ urlpatterns = [
     path('sign_up', views.signUp, name="sign_up"),
     path('homepage/<str:username>', views.index, name="home_page"),
     path('search', views.searchResult, name="search_result"),
-<<<<<<< HEAD
-=======
     path('getRelatedSearch/<str:search>', views.getRelatedSearch, name="getRelatedSearch"),
->>>>>>> e23a8420a8106f6e13960864a1833fb0825e092b
     path('edit_profile/<str:username>',views.editProfile, name="edit_profile")
 ]
