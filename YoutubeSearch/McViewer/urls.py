dfe25c7db8ad1e691cd727b115b13d2383662012
Chad <chad.spector@mail.mcgill.ca>
2020-06-07 02:04:44 -0400

    added default.jpg

--- YoutubeSearch/McViewer/urls.py
+++ YoutubeSearch/McViewer/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # path('login', views.login, name="login"),
     path('sign_up', views.signUp, name="sign_up"),
     path('homepage/<str:username>', views.index, name="home_page"),
-    path('search', views.searchResult, name="search_result"),
+    path('search/<str:search>', views.searchResult, name="search_result"),
     path('getRelatedSearch/<str:search>', views.getRelatedSearch, name="getRelatedSearch"),
     path('edit_profile/<str:username>',views.editProfile, name="edit_profile")
 ]
